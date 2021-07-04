c47aa70cfe8b5167eb3a89e3804ee1963fd3fb06
moominzie <yammybbraimong@gmail.com>
2021-07-04 15:02:51 +0700

    Edit Admin Header

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -61,10 +61,7 @@
 
 
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">  
-      <li class="nav-item">
-        <a class="nav-link" href="dashboard.php">Dashboard <span class="sr-only">(current)</span></a>
-      </li>    
+    <ul class="navbar-nav">    
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Add product
